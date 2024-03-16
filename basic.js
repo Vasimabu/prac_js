5f1eaf49ade3e946d9adca5af3957486b47233c0
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-03-16 16:37:59 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,42 @@
+/*var number =prompt("enetr any number :")
+if(number%2==0)
+{
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}*/
+//citizen ship logic
+/*var age = prompt("enetr yoir age :")
+if(age>=60)
+{
+    console.log("first priority")//its also display the answer in console 
+    alert("first")//also display the anser
+}
+else if(age>=40){
+    console.log("second priority") 
+    alert("second")
+}
+else{
+    console.log("ypur not eligible")
+    alert("not eligible")
+}*/
+/*var name =prompt("enter your name")
+console.log(name.length)*/
+//array basic program
+/*var name =prompt("enter your name:")
+console.log(name.length)
+i=0
+while(i<name.length){
+console.log(name[i])
+ i++
+}*/
+/*var num=prompt("enter the divisor of number:")
+console.log("the divisor no is"+num)
+i=0
+while(i<=num){
+    if(num%i==0){
+        console.log(i)
+    }
+i++
+}*/
