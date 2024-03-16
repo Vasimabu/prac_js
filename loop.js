32a443dd75c9c1cfa7e8de68764e4fce7825414b
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-03-16 16:43:45 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,100 @@
+//problems of js
+//1. 1 to 100 numbers divisble by 2
+/*var num=100
+i=0
+while(i<=num){
+    if(i%2==0){
+        console.log(i)
+    }
+    i++
+}*/
+//2.1 to 100 numbers divisble by 3
+/*var num=100
+i=0
+while(i<=num){
+    if(i%3==0){
+        console.log(i)
+    }
+    i++
+}*/
+//3. 1 to 100 numbers divisble 2 and 3
+// var num=100
+// i=0
+// while(i<=num){
+//     if(i%2==0 && i%3==0)
+//     {
+//         console.log(i)
+//     }
+//     i++
+// }
+//4. 1 to 100 numbers divisble 2 or 3
+/*var num=100
+i=0
+while(i<=num){
+    if(i%2==0 || i%3==0)
+    {
+        console.log(i)
+    }
+    i++
+}*/
+//5. total count in first 100 numbets
+/*var num=100
+i=0
+count=0
+while(i<=num){
+    count=i+count
+    i++
+}
+console.log(count)*/
+//6. prime number program
+/*no=10
+isprime=true
+if(no<1){
+    console.log("not prime")
+}
+else if(no>1){
+    i=2
+    while(i<no){
+        if(no%i==0){
+        isprime=false
+        break
+    }
+    i++
+   }
+   if(isprime==true){
+    console.log("prime")
+   }
+   else{
+    console.log("not prime")
+   }
+}
+else{
+    console.log("not a prime")
+}*/
+/*if(prime==1){
+    console.log("prime")
+}*/
+//loping
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
