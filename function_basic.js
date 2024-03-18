c71177d4cd50447582eb02f08e628e67b0b6bd4d
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-03-18 12:08:13 +0530

    Add files via upload

---
+++ function_basic.js
@@ -0,0 +1,24 @@
+//functions
+// function wish(name){
+//     console.log("all the best"+name)
+// }
+// name=prompt("enter your name")
+// wish(name)
+// aruguments name was change
+// function wish(abc){
+//     console.log("all the best"+abc)
+// }
+// name=prompt("enter your name")
+// wish(name)
+//aruguments and valies are change
+/*function repay(amount){
+    console.log("you repaid" + amount)
+}
+salary=10000
+repay(salary)*/
+//muliply with return statement,this method also outside the values
+function multiply(no1,no2){
+    return no1*no2
+}
+var result=multiply(10,20)
+console.log(result)
