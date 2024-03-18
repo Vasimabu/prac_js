cc253627f94c0d242f4744229c98503d1e7571a7
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-03-18 13:37:25 +0530

    Add files via upload

---
+++ array_basic.js
@@ -0,0 +1,7 @@
+//array basic
+var actors=["vijay",0.2,"dhnaush","surya"]
+console.log(actors[0])
+console.log(actors[1])
+console.log(actors[2])
+//heterogeneous elements means multiple data types
+//homogeneous means single type of data
