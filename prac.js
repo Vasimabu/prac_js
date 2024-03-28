b6a3273bae35f5a9f6d7bf876e1c3a276c26e9a0
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-03-28 14:39:24 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,70 @@
+//Youare given a number A in Kilometers. Convert this into B: Meters and C: Centi-Metres.
+// Input Description:
+// A number "A" representing some distance in kilometer is provided to you as the input.
+// Output Description:
+// Convert and print this value in meters and centimeters. 
+/*let A = Number(prompt("enter the number :"))
+let B = A*1000
+let C = A*100000
+console.log(B)
+console.log(C)*/
+
+// You are given A = Length of a rectangle & B = breadth of a rectangle. Find its area “C”.
+// (A and B are natural numbers)
+// Input Description:
+// The inputs are two natural numbers representing the length and the breadth of a rectangle.
+// Output Description:
+// Find the area of the rectangle formed by the provided input. Round off the answer to the first decimal place if required.
+/*let A = Number(prompt("length of rectangle :"))
+let B = Number(prompt("breadth of rectangle :"))
+let C = A*B
+console.log(Math.round(C))*/
+
+// You are provided with the radius of a circle "A". Find the length of its circumference.
+// Note: In case the output is coming in decimal, roundoff to 2nd decimal place. In case the input is a negative number, print "Error".
+// Input Description:
+// The Radius of a circle is provided as the input of the program.
+// Output Description:
+// Calculate and print the Circumference of the circle corresponding to the input radius up to two decimal places.
+/*let A=Number(prompt("enetr the radius"))
+let c=2*3.14*A
+if(c<0){
+    console.log("error")
+}
+else{
+    console.log(c.toFixed(Math.round(2)))
+}*/
+
+// You are provided with a number, "N". Find its factorial.
+// Input Description:
+// A positive integer is provided as an input.
+// Output Description:
+// Print the factorial of the integer.
+/*let N=Number(prompt("enter the number"))
+let fact=1
+i=1
+while(N>=i){
+    fact=fact*i
+    i+=1
+}
+console.log(fact);
+*/
+
+// You are given Two Numbers, A and B. If C = A + B. Find C.
+// Note: Round off the output to a single decimal place.
+// Input Description:
+// You are provided with two numbers A and B.
+// Output Description:
+// Find the sum of the two numbers (A + B)
+/*let A = Number(prompt("length of rectangle :"))
+let B = Number(prompt("breadth of rectangle :"))
+let C = A+B
+console.log(C.toFixed(0))*/
+
+
+
+
+
+
+
+
