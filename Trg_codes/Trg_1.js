d3bdb647aadc59cd6cd993c611e8d23aeb9dc40f
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-04-25 16:39:33 +0530

    Add files via upload

---
+++ Trg_codes/Trg_1.js
@@ -0,0 +1,60 @@
+//1. odd or even numbers
+/*let num=Number(prompt("enter the number : "))
+if(num%2==0)
+    console.log("the given no",num,"is even");
+else
+    console.log("the given no",num,"is odd");
+*/
+//2. child or teenage or adult
+/*let age=Number(prompt("enter the age : "))
+if(age<13)
+    console.log("your a child");
+else if(age<19)
+     console.log("your Teenage");
+else  {  
+     console.log("your adults");
+}*/
+ //3. print even number 1 to 10
+ /*let num=Number(prompt("enter the number : "))
+ i=1
+ while(i<=num){
+    if(i%2==0)
+         console.log(i);
+    i+=1
+ } */
+ // 4. check if number is 1 to 10
+ /*let num=Number(prompt("enter the number : "))
+ if(num>=1 && num<=10)
+      console.log("the num is 1 to 10");
+else
+     console.log("error");
+*/
+// 5. if a number is positive or negative or zero
+/*let num=Number(prompt("enter the number : "))
+if(num==0)
+     console.log("the number is zero");
+else if(num<0)
+     console.log("the given no is negative");
+else
+    console.log("the given no is polsitive");
+*/
+//6. factorial
+/*let num=Number(prompt("enter the number : "))
+count=1
+for(i=1;i<=num;i++)
+     count=i*count
+console.log(count);*/
+
+//7. print a table(math)
+/*let num=Number(prompt("enter the number : "))
+for(i=1;i<=num;i++)
+     console.log(i +" * "+ num +" = "+i*num);
+*/
+//8. number from 1 to 10
+/*let num=Number(prompt("enter the number : "))
+for(i=1;i<=num;i++)
+      console.log(i);*/
+
+//9. concatenate two strings
+
+//10. summing of arrays
