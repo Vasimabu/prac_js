c2539c536a71e2e552f65c0eb066bb8394a98dc5
Vasimabu <117739650+Vasimabu@users.noreply.github.com>
2024-04-29 14:01:17 +0530

    Update array_basic.js

--- array_basic.js
+++ array_basic.js
@@ -21,6 +21,44 @@ console.log(cities)
 console.log(cities.unshift("kodai"))
 console.log(cities)
 delete cities[2]
+/*console.log(cities)
+// delete alternative method is splice
+//splice
+console.log(cities.splice(2,1))// 2means 2 elemtb in 1position
+console.log(cities)
+console.log(cities.splice(1,1,'m'))//delet and after replace
+console.log(cities)
+console.log(cities.splice(1,2,'x','y'))//delete 2values and alternative to insert 2values
+console.log(cities)
+console.log(cities.splice(1,0,'b'))//no deletion insert 1st position values
+console.log(cities)*/
+//slice method
+/*console.log(cities.slice(1,2))
+console.log(cities)
+console.log(cities.reverse())
+console.log(cities)
+let str=cities.join()//convert into string
+console.log(str)
+//split -converts string to arrary
+let str3="g,t,j,r"
+let arr3=str3.split(',')
+console.log(arr3)*/
+
+//arrays in depth concept
+
+//adding elemnt in array
+/*const n=[4,5,6]
+console.log(n)
+
+let user=prompt("enter the fruits")
+let a=["apple","banana"]
+a.push("gauva")
+for(i=0;i<1;i++)
+     a.unshift(user)
+console.log(a);*/
+
+
+
 console.log(cities)
 // delete alternative method is splice
 //splice
